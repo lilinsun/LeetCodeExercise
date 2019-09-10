904b57c4b49fddbe649b2274f7e8ad71dbbcb0bf
linsun.li <qc_c1e2c0b1da1e4c298ecb711b8dbe1262@git.code.tencent.com>
2019-09-10 11:31:51 +0800

    初始化Maven项目

---
+++ src/main/java/cn/lls/leetcodeexercise/LeetcodeexerciseApplication.java
@@ -0,0 +1,13 @@
+package cn.lls.leetcodeexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeexerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeexerciseApplication.class, args);
+    }
+
+}
