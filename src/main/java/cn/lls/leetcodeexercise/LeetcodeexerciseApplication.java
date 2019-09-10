60c2669da9666aad2693e9aac2860d923294c619
linsun.li <qc_c1e2c0b1da1e4c298ecb711b8dbe1262@git.code.tencent.com>
2019-09-10 12:10:45 +0800

    题1.TwoSum

--- src/main/java/cn/lls/leetcodeexercise/LeetcodeexerciseApplication.java
+++ src/main/java/cn/lls/leetcodeexercise/LeetcodeexerciseApplication.java
@@ -3,6 +3,11 @@ package cn.lls.leetcodeexercise;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * LeetCodeExercise
+ *
+ * @author LLS
+ */
 @SpringBootApplication
 public class LeetcodeexerciseApplication {
 
---
+++ src/main/java/cn/lls/leetcodeexercise/leetcode/TwoSum.java
@@ -0,0 +1,51 @@
+package cn.lls.leetcodeexercise.leetcode;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * <p>
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@Slf4j
+public class TwoSum {
+
+    private static int[] twoSum(int[] nums, int target) {
+        int[] index = new int[2];
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (hashMap.containsKey(temp)) {
+                index[0] = i;
+                index[1] = hashMap.get(temp);
+                return index;
+            }
+            hashMap.put(nums[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 5, 5, 15};
+        int target = 10;
+
+        int[] result = twoSum(nums, target);
+
+        log.info("输出结果为：{}", result);
+    }
+}
